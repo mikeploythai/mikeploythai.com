45b62c718d45c56a80cb459fee5bed48398e0655
mikeploythai <54593375+mikeploythai@users.noreply.github.com>
2022-05-26 15:51:40 -0700

    Refactored header component

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import styles from '../styles/Header.module.css'
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <div className={styles.container}>
+        <a className={styles.init} href="#">
+          <b>MP</b>
+        </a>
+
+        <a
+          className={styles.btn}
+          href="#"
+          target="_blank"
+          rel="noopenner noreferrer"
+        >
+          Check out my resume!
+        </a>
+      </div>
+    </div>
+  )
+}
