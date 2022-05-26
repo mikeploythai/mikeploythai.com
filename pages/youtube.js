ae469ff000d70cdd94208c3eda714c4a135c9a3d
mikeploythai <54593375+mikeploythai@users.noreply.github.com>
2022-05-26 15:52:57 -0700

    Created YouTube link

---
+++ pages/youtube.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function YouTube() {
+  return (
+    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh'}}>
+      <Head>
+        <title>Redirecting...</title>
+        <meta http-equiv="refresh" content="0; url=https://youtube.com/channel/UCCM4FmUaCVo2928Yvg-G8rA" />
+      </Head>
+
+      <h1>Redirecting...</h1>
+    </div>
+  )
+}
