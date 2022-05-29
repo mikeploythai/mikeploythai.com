f52abc38666cd4125d7d5b32d3d9ead678ef297d
mikeploythai <54593375+mikeploythai@users.noreply.github.com>
2022-05-29 14:01:47 -0700

    Added individual keys

--- components/LinkGrid.js
+++ components/LinkGrid.js
@@ -4,9 +4,9 @@ import styles from '../styles/LinkGrid.module.css'
 export default function LinkGrid() {
   return (
     <div className={styles.list}>
-      {links.map(({icon, link}) =>(
+      {links.map(({icon, link, key}) =>(
         <a
-          key={icon}
+          key={key}
           className={styles.icons}
           href={link}
           target="_blank"
--- components/lib/links.js
+++ components/lib/links.js
@@ -1,18 +1,22 @@
 export const links = [
   {
     icon: <ion-icon name="mail-outline"></ion-icon>,
-    link: "mailto:hello@mikeploythai.com"
+    link: "mailto:hello@mikeploythai.com",
+    key: 1
   },
   {
     icon: <ion-icon name="logo-github"></ion-icon>,
-    link: "https://github.com/mikeploythai"
+    link: "https://github.com/mikeploythai",
+    key: 2
   },
   {
     icon: <ion-icon name="logo-linkedin"></ion-icon>,
-    link: "https://linkedin.com/in/mikeploythai"
+    link: "https://linkedin.com/in/mikeploythai",
+    key: 3
   },
   {
     icon: <ion-icon name="logo-instagram"></ion-icon>,
-    link: "https://instagram.com/mikeploythai"
+    link: "https://instagram.com/mikeploythai",
+    key: 4
   },
 ]
